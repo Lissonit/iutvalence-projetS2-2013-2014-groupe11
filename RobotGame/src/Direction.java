03423592224ee1cd8b0bd6e0e8b27e75fc52d7ec
Clément <Clment@Clment-PC>
2014-05-14 21:06:02 +0200

    Ajout du constructeur et des coordonnées pour l'énumeration Direction

--- RobotGame/src/Direction.java
+++ RobotGame/src/Direction.java
@@ -1,8 +1,38 @@
 /* Enumération directions */
 public enum Direction 
 {
-	HAUT,
-	BAS,
-	GAUCHE,
-	DROITE
-}
+	HAUT(0, -1),
+	BAS(0, 1),
+	GAUCHE(-1, 0),
+	DROITE(1, 0);
+	
+	private int X;
+	private int Y;
+	
+	/**
+	 * Nouvelle direction
+	 * @param x coordonnée x de la direction
+	 * @param y coordonnée y de la direction
+	 */
+	Direction(int x, int y)
+	{
+		this.X = x;
+		this.Y = y;
+	}
+	
+	/**
+	 * Obtention de la coordonnée X de la dirction
+	 * @return La coordonnée X (Horizontale)
+	 */
+	public int obtenirX() {
+		return X;
+	}
+
+	/**
+	 * Obtention de la coordonnée Y de la dirction
+	 * @return La coordonnée Y (Verticale)
+	 */
+	public int obtenirY() {
+		return Y;
+	}
+};
