4fd360ba4223c16b8234f21b27760300cac3a36a
Clément <Clment@Clment-PC>
2014-05-14 23:06:31 +0200

    Création de l'exception OutOfRange

---
+++ RobotGame/src/OutOfRangeException.java
@@ -0,0 +1,10 @@
+
+public class OutOfRangeException extends Exception 
+{
+	private static final long serialVersionUID = 1L;
+
+	public OutOfRangeException(String message)
+	{
+		super(message);
+	}
+}
