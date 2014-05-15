010ba6bcc696083af8579a58a35c0b4a803bf934
Clément <Clment@Clment-PC>
2014-05-15 13:57:11 +0200

    Bases de lancement

--- RobotGame/src/Application.java
+++ RobotGame/src/Application.java
@@ -1,9 +1,13 @@
 
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Création de l'instance de jeu");
+		IHMJoueur jeu = new IHMJoueur();
+		System.out.println("Lancement de la partie");
+		jeu.lancer();
+		System.out.println("Fin de la partie");
 	}
 
 }
